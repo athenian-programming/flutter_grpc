96267c4edbf346a669f39a3d08bac873712180f0
Paul Ambrose <pambrose@mac.com>
2019-02-24 11:42:35 -0800

    Cleanup server msg

--- lib/server.dart
+++ lib/server.dart
@@ -2,7 +2,8 @@ import 'package:flutter_helloworld/greeter_impl.dart';
 import 'package:grpc/grpc.dart';
 
 Future<Null> main(List<String> args) async {
+  const port = 50051;
   final server = Server([new GreeterService()]);
-  await server.serve(port: 50051);
-  print('Server listening...');
+  await server.serve(port: port);
+  print('Server listening on port $port...');
 }
